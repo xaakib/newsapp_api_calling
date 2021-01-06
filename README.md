90c88208c9e7e8cacbc40d02595133374320cceb
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-01-06 12:39:57 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# newsapp_api_calling
+ 
---
+++ android/app/src/main/kotlin/com/example/newsapp_api_calling/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.newsapp_api_calling
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
