f05b3966635317252a7197fed87ea3997c0d4628
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-01-23 15:05:58 +0600

    category are done

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:newsapp_api_calling/helper/data.dart';
+import 'package:newsapp_api_calling/model/categori_model.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -6,6 +8,13 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List<CategorieModel> categories = List<CategorieModel>();
+  @override
+  void initState() {
+    super.initState();
+    categories = getCategories();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,28 +35,84 @@ class _HomeState extends State<Home> {
         centerTitle: true,
       ),
       body: Container(
-        // child: CategoryTile(),
+        child: Column(
+          children: [
+            Container(
+              height: 70,
+              margin: EdgeInsets.symmetric(horizontal: 16),
+              child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: categories.length,
+                  physics: BouncingScrollPhysics(),
+                  itemBuilder: (context, index) {
+                    return CategoryTile(
+                      imageUrl: categories[index].imageAssetUrl,
+                      categoryName: categories[index].categorieName,
+                    );
+                  }),
+            )
+          ],
+        ),
       ),
     );
   }
 }
 
-
 class CategoryTile extends StatelessWidget {
-  final imageUrl,categoryName;
+  final imageUrl, categoryName;
 
   const CategoryTile({Key key, this.imageUrl, this.categoryName});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-
+      margin: EdgeInsets.only(right: 16),
       child: Stack(
         children: [
-          Image.network(imageUrl,width: 120,height: 60,),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(6),
+            child: Image.network(
+              imageUrl,
+              width: 120,
+              height: 60,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            width: 120,
+            height: 60,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(6),
+              color: Colors.black26,
+            ),
+            child: Text(
+              categoryName,
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.w500),
+            ),
+          )
         ],
       ),
-      
+    );
+  }
+}
+
+class BlogTile extends StatelessWidget {
+  final String imageUrl, title, desc;
+
+  const BlogTile({Key key, this.imageUrl, this.title, this.desc});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Image.network(imageUrl),
+          Text(title),
+          Text(desc),
+        ],
+      ),
     );
   }
 }
